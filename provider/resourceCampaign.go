e655bee1d28170d0107420f3d47d000ab7f92f2f
Andre Ferreira Lima <andre.lima@grupoltm.com.br>
2019-02-21 16:44:37 -0300

    new clients

--- provider/resourceCampaign.go
+++ provider/resourceCampaign.go
@@ -2,30 +2,12 @@ package provider
 
 import (
 	"fmt"
-	"regexp"
 	"strings"
 
 	"github.com/aferlim/terraform-provider-example/client/campaign"
 	iacitem "github.com/aferlim/terraform-provider-example/client/iac-item"
 	"github.com/hashicorp/terraform/helper/schema"
 )
-
-func validateName(v interface{}, k string) (ws []string, es []error) {
-	var errs []error
-	var warns []string
-	value, ok := v.(string)
-	if !ok {
-		errs = append(errs, fmt.Errorf("Expected name to be string"))
-		return warns, errs
-
-	}
-	whiteSpace := regexp.MustCompile(`\s+`)
-	if whiteSpace.Match([]byte(value)) {
-		errs = append(errs, fmt.Errorf("name cannot contain whitespace. Got %s", value))
-		return warns, errs
-	}
-	return warns, errs
-}
 
 func resourceCampaign() *schema.Resource {
 	fmt.Print()
